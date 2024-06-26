10006eedd3fa604caa67e7851ab3cfe90858d31f
Cocoa <i@uwucocoa.moe>
2024-06-26 19:52:29 +0100

    fix ci

--- .github/scripts/stage1.sh
+++ .github/scripts/stage1.sh
@@ -6,7 +6,7 @@ PYTHON3_VERSION=$1
 HOST_ARCH=$2
 HOST_ABI=$3
 DOCKER_IMAGE=$4
-DOCKER_PLATFORM=$4
+DOCKER_PLATFORM=$5
 
 sudo docker run --privileged --network=host --rm --platform="${DOCKER_PLATFORM}" -v $(pwd):/work "${DOCKER_IMAGE}" \
     sh -c "chmod a+x /work/stage2.sh && /work/stage2.sh ${PYTHON3_VERSION} ${HOST_ARCH}-linux-${HOST_ABI}"
