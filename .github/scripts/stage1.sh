050c87b39dfab3bdb57956ca715df44080d1764a
Cocoa <i@uwucocoa.moe>
2024-06-26 19:49:53 +0100

    fix ci

--- .github/scripts/stage1.sh
+++ .github/scripts/stage1.sh
@@ -6,8 +6,9 @@ PYTHON3_VERSION=$1
 HOST_ARCH=$2
 HOST_ABI=$3
 DOCKER_IMAGE=$4
+DOCKER_PLATFORM=$4
 
-sudo docker run --privileged --network=host --rm --platform="linux/${HOST_ARCH}" -v $(pwd):/work "${DOCKER_IMAGE}" \
+sudo docker run --privileged --network=host --rm --platform="${DOCKER_PLATFORM}" -v $(pwd):/work "${DOCKER_IMAGE}" \
     sh -c "chmod a+x /work/stage2.sh && /work/stage2.sh ${PYTHON3_VERSION} ${HOST_ARCH}-linux-${HOST_ABI}"
 
 if [ -d "$(pwd)/build" ]; then
